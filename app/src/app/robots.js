dcfa1d22d6a789f26cb393db71dccab968feb50e
thinkfashz <faubricioedms@gmail.com>
2026-09-10 19:46:23 -0300

    feat(seo): add robots metadata route

---
+++ app/src/app/robots.js
@@ -0,0 +1,14 @@
+const SITE_URL = 'https://sazon-y-sabor-phi.vercel.app';
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: `${SITE_URL}/sitemap.xml`,
+    host: SITE_URL,
+  };
+}
